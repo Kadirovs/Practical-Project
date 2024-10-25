09be87843056bdb9ebb6fb580c625d85b2d73395
Kadirov Kamoliddin <kadirovkamoliddin24@gmail.com>
2024-10-25 18:26:13 +0400

    Practice Project

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    
+
+<div class= maindiv>
+    <div class= div1>
+        <img src="download (1).png" alt=""> 
+        <?php
+        $amount = 2500;
+
+        echo "<p class= p1>", $amount , "</p>";
+        ?>
+        <h3> Welcome </h3>
+
+    </div>
+    <div class= div1>
+        <img src="download.png" alt=""> 
+        <?php
+        $am = 123.50;
+        echo "<p class= p1>", $am , "</p>";
+        ?>
+        <h3> Average time </h3>
+    </div>
+    <div class= div1>
+        <img src="images.jpg" alt=""> 
+        <?php
+        $amo = 1.805;
+        echo "<p class= p1>", $amo , "</p>";
+        ?>
+        <h3> Collections </h3>
+    </div>
+    <div class= div1>
+        <img src="images.png" alt=""> 
+        <?php
+        $unt = 54;
+        echo "<p class= p1>", $unt , "</p>";
+        ?>
+        <h3> Comments </h3>
+</div>
+
+
+    
+    
+    
+</div>
+    
+    
+</body>
+</html>
